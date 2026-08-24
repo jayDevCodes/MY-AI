09b2980cb428b95ea8c31899a10ecb83ddb1bc88
Jay Dev <jaydev787748@gmail.com>
2026-08-24 18:32:06 +0530

    feat(v7): expose recursive graph and code intelligence APIs

--- src/myai/__init__.py
+++ src/myai/__init__.py
@@ -1,5 +1,7 @@
-"""MY-AI V6 public package."""
+"""MY-AI V7 public package."""
 
+from .agent_graph import ExecutionBudget, RecursiveAgentGraph, TaskNode, WorkArtifact
+from .code_intelligence import CodeFile, CodeIntelligenceIndex, CodeSymbol
 from .engine import AIEngine
 from .knowledge import (
     Document,
@@ -8,15 +10,26 @@ from .knowledge import (
     SQLiteVectorStore,
     chunk_text,
 )
+from .model_router import AdaptiveModelRouter, RoutingDecision, RoutingRequest
 from .schemas import ChatRequest, ChatResponse
 
 __all__ = [
     "AIEngine",
+    "AdaptiveModelRouter",
     "ChatRequest",
     "ChatResponse",
+    "CodeFile",
+    "CodeIntelligenceIndex",
+    "CodeSymbol",
     "Document",
+    "ExecutionBudget",
     "InMemoryKnowledgeStore",
+    "RecursiveAgentGraph",
     "RetrievedChunk",
+    "RoutingDecision",
+    "RoutingRequest",
     "SQLiteVectorStore",
+    "TaskNode",
+    "WorkArtifact",
     "chunk_text",
 ]
